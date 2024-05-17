d1127ad1d36d19860c97429d2df49f38c37477de
Suryanshu <tshuryanshu@gmail.com>
2024-05-18 00:25:49 +0530

    Updates

---
+++ precache-manifest.6c70402ef2d1f2eaecc3c76e6f9e8a2e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddad9bd2500d084712f6e1e44a65ab0",
+    "url": "/Sorting-Visualizer/*/index.html"
+  },
+  {
+    "revision": "b5cc9a537d18ffd2bb71",
+    "url": "/Sorting-Visualizer/*/static/css/main.bdfce58a.chunk.css"
+  },
+  {
+    "revision": "6cc14f0a0b65309618e7",
+    "url": "/Sorting-Visualizer/*/static/js/2.ebc4d5f9.chunk.js"
+  },
+  {
+    "revision": "ace03e8d620bb0ad5f7f2a8dfdc9d75d",
+    "url": "/Sorting-Visualizer/*/static/js/2.ebc4d5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cc9a537d18ffd2bb71",
+    "url": "/Sorting-Visualizer/*/static/js/main.e18f00ea.chunk.js"
+  },
+  {
+    "revision": "5245246b0ea8d04d4cfc",
+    "url": "/Sorting-Visualizer/*/static/js/runtime-main.320c4a53.js"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/Sorting-Visualizer/*/static/media/Anurati-Regular.ca8632f4.otf"
+  },
+  {
+    "revision": "bf570edf85c826128ac4fd5b4dcbba7f",
+    "url": "/Sorting-Visualizer/*/static/media/analytics.bf570edf.svg"
+  },
+  {
+    "revision": "4c836d2b5448621906f6b5f9bb77bc8a",
+    "url": "/Sorting-Visualizer/*/static/media/bar-chart.4c836d2b.svg"
+  },
+  {
+    "revision": "f6b7f9acf5affd2144eb172d62cd325e",
+    "url": "/Sorting-Visualizer/*/static/media/convex.f6b7f9ac.png"
+  },
+  {
+    "revision": "cfd6557ef662cce8ba82b98cda683394",
+    "url": "/Sorting-Visualizer/*/static/media/convex_instruct.cfd6557e.gif"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/Sorting-Visualizer/*/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "eb2a63975cdaa68094fe7f8bed132ec3",
+    "url": "/Sorting-Visualizer/*/static/media/heading_logo.eb2a6397.png"
+  },
+  {
+    "revision": "afc106ad260cdbb93ce12d296a167cc4",
+    "url": "/Sorting-Visualizer/*/static/media/linkedin.afc106ad.svg"
+  },
+  {
+    "revision": "67aa2ccbada71528e9c6ccd05917cef8",
+    "url": "/Sorting-Visualizer/*/static/media/pathfinder.67aa2ccb.gif"
+  },
+  {
+    "revision": "a938ed3f872ba49d8cc3668e80078975",
+    "url": "/Sorting-Visualizer/*/static/media/pathfinding_front.a938ed3f.svg"
+  },
+  {
+    "revision": "2f3f3bdbd031928bf064c47c18e0ecca",
+    "url": "/Sorting-Visualizer/*/static/media/planet.2f3f3bdb.svg"
+  },
+  {
+    "revision": "24f618090c99aa8e9fc203ebd46484e9",
+    "url": "/Sorting-Visualizer/*/static/media/sorting.24f61809.png"
+  },
+  {
+    "revision": "4db25bca510cd839d849d71904990ba0",
+    "url": "/Sorting-Visualizer/*/static/media/space-ship.4db25bca.svg"
+  }
+]);
